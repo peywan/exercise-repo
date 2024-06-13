d97f3d6777f7b9ded42ffda5a695176514839b12
Peywan Sabir <peywan1999@icloud.com>
2024-06-13 11:43:58 +0200

    ternary operator

---
+++ script.js
@@ -0,0 +1,50 @@
+/* let newUser = [];
+
+newUser.push("hello new user, welcome.")
+
+console.log(newUser)
+
+
+// Step 1: Declare a variable 'name' and assign it the value "John"
+let name = "john"
+
+// Step 2: Declare a variable 'admin'
+let admin = "admin"
+
+// Step 3: Copy the value from 'name' to 'admin'
+admin = name
+
+// Step 4: Use alert to show the value of 'admin'
+
+console.log(admin)
+//num2
+// Task 1: Declare a variable for the name of our planet
+let earth = "earth planet"
+
+// Task 2: Declare a variable for the name of the current visitor to a website
+let currentVisitor = "john doe"
+
+*/
+
+let name = "ilya";
+
+
+let age = prompt('age?', 18);
+
+let message = (age < 3) ? 'Hi, baby!' :
+    (age < 18) ? 'Hello!' :
+        (age < 100) ? 'Greetings!' :
+            'What an unusual age!';
+
+alert(message);
+
+
+
+// Step 1: Declare a variable 'age' and assign it a number value
+
+// Step 2: Declare a variable 'canVote'
+
+// Step 3: Use the ternary operator to set 'canVote' to true if 'age' is 18 or older, and false otherwise
+
+// Step 4: Use console.log to display whether the person can vote or not based on the value of 'canVote'
+
